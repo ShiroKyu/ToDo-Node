3b0ece7e6ef459585893ceb593337086eb21d56e
Paulo <paulo.leite@academico.ifpb.edu.br>
2020-07-02 19:31:34 -0300

    Botão de excluir ToDo configurado

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,23 @@
 let formulario = document.querySelector('form#adicionarTodo')
+
 let todoInput = document.querySelector('input.addtodo')
 let todo_area = document.querySelector('.todo-area')
 
+let aaa = document.createElement('form')
+console.log(aaa)
 
 let todo = ''
-formulario.addEventListener('submit', (e) => {
+
+formulario.addEventListener('submit', e => {
     todo = todoInput.value
-    console.log(todo)
+
     e.preventDefault()
 
     let newForm = document.createElement('form')
     let newInput = document.createElement('input')
-    let newBtn = document.createElement('button')
+    let delBtn = document.createElement('button')
+
+    newForm.classList.toggle('resultForm')
 
     //Configurando o input
     newInput.classList.toggle('resultTodo')
@@ -19,10 +25,22 @@ formulario.addEventListener('submit', (e) => {
     newInput.value = todo
     newInput.disabled = true
 
-    //COnfigurando o button
-    newBtn.classList.toggle('resultBtn')
+    //Configurando o button
+    delBtn.classList.toggle('btn-submit')
+    delBtn.type = 'button'
 
+    let trashImg = document.createElement('img')
+    trashImg.src = 'assets/trash.svg'
+    trashImg.width = '20'
+
+    delBtn.appendChild(trashImg)
 
     newForm.appendChild(newInput)
+    newForm.appendChild(delBtn)
     todo_area.appendChild(newForm)
+
+    //Configurando botão de excluir toDo
+    delBtn.addEventListener('click', e => {
+        newForm.remove()
+    })
 })
